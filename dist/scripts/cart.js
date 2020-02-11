543e0a043170ccd74012db48c1761e6589fe95cd
ramy <ramy.niranjan@gmail.com>
2020-02-11 13:38:04 +0100

    finish version-1

--- dist/scripts/cart.js
+++ dist/scripts/cart.js
@@ -1,15 +1,155 @@
+
+
+let cartText=document.querySelector('.cart__text')
+let wrapper = document.querySelector('.wrapper')
+let sum = document.querySelector('.sum')
+let cartCount = document.querySelector('.nav__cart-count')
+let empty = document.querySelector('.cart__empty p')
+let cartResult = document.querySelector('.cart__result')
+
+getCartData()
+getTotal()
+
 function getTotal() {
-  let cartCount = document.querySelector('.nav__cart-count')
   let total = localStorage.getItem('totalProd')
-  console.log(total)
   cartCount.innerHTML = total
 }
-getTotal()
+
+
+
+
+function calSum(){
+  let cart = JSON.parse(localStorage.getItem('cart'))
+ return cart.flat().reduce((total,curr)=>{
+   total += parseInt(curr.price)
+   return total
+  },0)}
+
+function creatingArr() {
+  let data = JSON.parse(localStorage.getItem('cart'))
+  return data.reduce((arr) => {
+    arr.push(new Array)
+    return arr
+  }, [])
+
+}
+  
+empty.addEventListener('click',(e)=>{
+ 
+  localStorage.setItem('cart', JSON.stringify(creatingArr()))
+  localStorage.setItem('totalProd', JSON.stringify(0))
+  let element = document.querySelector('.cart__result');
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  cartCount.innerHTML=0
+  wrapper.style.display = 'none'
+  cartText.style.display = 'block'
+})
+
 
 
 function getCartData(){
   let cart=JSON.parse(localStorage.getItem('cart'))
   console.log(cart)
+  
+  if(!cart){
+    cartText.style.display = 'block'
+  }else{
+    for (let i = 0; i < cart.length; i++) {
+
+      if (cart[i].length) {
+        wrapper.style.display = 'block'
+        populateDom(cart[i][0], cart[i], i)
+        sum.innerHTML = calSum()
+      } else if (!cart.flat().length) {
+
+        cartText.style.display = 'block'
+      }
+
+    }
+  }
+
 }
 
-getCartData()
+
+
+
+  let close = document.querySelectorAll('.cart__products-close')
+  close.forEach(item=>{
+    
+    item.addEventListener('click', (e) => {
+      let cart = JSON.parse(localStorage.getItem('cart'))
+      cart[e.target.parentElement.id]=[]
+      localStorage.setItem('cart',JSON.stringify(cart))
+      localStorage.setItem('totalProd', cart.flat().length)
+      cartCount.innerHTML = localStorage.getItem('totalProd')
+      sum.innerHTML = calSum()
+      e.target.parentElement.remove()
+      console.log(cart)
+      if(!cart.flat().length){
+        wrapper.style.display = 'none'
+        cartText.style.display = 'block'
+      }
+    })
+  })
+  
+
+
+
+let input=document.querySelectorAll('.qty')
+input.forEach(item=>{
+  item.addEventListener('input',(e)=>{
+    let id = e.target.parentElement.parentElement.id
+    let cart = JSON.parse(localStorage.getItem('cart'))
+    localStorage.setItem('totalProd', cart.flat().length)
+    let temArr=[]
+    // if (e.target.value==0){
+    //   setTimeout(() => {
+    //     if(e.target.value == 0){
+    //       e.target.parentElement.parentElement.remove()
+    //     } }, 3000);
+    //    }
+
+    if (e.target.value){
+      for (let i = 0; i < e.target.value ; i++) {
+        temArr.push(cart[id][0])
+      }
+      cart[id]=temArr
+      localStorage.setItem('cart',JSON.stringify(cart))
+      localStorage.setItem('totalProd', JSON.stringify(cart.flat().length))
+      cartCount.innerHTML = localStorage.getItem('totalProd')
+      sum.innerHTML = calSum()
+    }
+  })
+  
+})
+
+
+
+
+function populateDom(data=null,total=null,id=null){
+  let result = document.querySelector('.cart__result')
+
+  let html = ` <div id="${id}"class="cart__products">
+            <div class="cart__products-wrapper">
+              <img src="${data.image}" alt="" srcset="" />
+            </div>
+            <p class="cart__products-name">
+              ${data.name}
+            </p>
+            <div class="cart__products-close">
+              x
+            </div>
+            <p class="cart__products-price">
+              ${data.price}
+            </p>
+            <div class="cart__products-quntity">
+              <label for="qty">Qty</label>
+              <input class="qty" type="number" name="" id="qty" value="${total.length}" />
+            </div>
+          </div>`
+
+  result.innerHTML+=html
+}
+
--- dist/scripts/fetch.js
+++ dist/scripts/fetch.js
@@ -7,17 +7,31 @@
   }
 
   getData().then(data => {
-    // console.log(data)
-    localStorage.setItem('data', JSON.stringify(data))
-  })
-
-  function getProducts() {
-    let data = JSON.parse(localStorage.getItem('data'))
-    // console.log(data)
+    
+    let arr=[]
     let products = data.products
     products.forEach((item) => {
       upDateUi(item)
+      arr.push(new Array)
+      
     })
+    creatingArr(arr)
+    let search = document.querySelector('.search')
+    let productsCard = document.querySelectorAll('.products__card')
+    let cartCount = document.querySelector('.nav__cart-count')
+    let wrapper = document.querySelectorAll('.products__add-wrapper')
+    let arrCart = JSON.parse(localStorage.getItem('cart'))
+    let total = findingLength(arrCart)
+    addToCart(wrapper, cartCount, total,arr)
+    searchItems(search, productsCard)
+  })
+
+  function getProducts() {
+    // let data = JSON.parse(localStorage.getItem('data'))
+    // let products = data.products
+    // products.forEach((item) => {
+    //   upDateUi(item)
+    // })
   }
 
 
@@ -32,7 +46,7 @@
         <div class="products__price">${data.price}</div>
         <div class="products__add-wrapper">
           <div class="products__minus">-</div>
-          <div class="products___add">Add To Cart</div>
+          <div class="products___add">0</div>
           <div class="products__plus">+</div>
         </div>
       </div>`
@@ -41,7 +55,6 @@
 
   }
 
-  document.addEventListener('DOMContentLoaded', getProducts())
 
 
 
@@ -50,10 +63,6 @@
 
 
 
-let search = document.querySelector('.search')
-let products = document.querySelectorAll('.products__card')
-let cartCount = document.querySelector('.nav__cart-count')
-let wrapper = document.querySelectorAll('.products__add-wrapper')
 
 
 function getTotal() {
@@ -65,12 +74,6 @@ function getTotal() {
 getTotal()
 
 
-
-let arr = creatingArr()
-let total = findingLength(arr)
-
-
-
 function findingLength(arr){
   let total=0
    arr.forEach(item=>{
@@ -80,13 +83,85 @@ function findingLength(arr){
 }
 
 
-function creatingArr() {
-  let data = JSON.parse(localStorage.getItem('data')).products
-  return data.reduce((arr) => {
-    arr.push(new Array)
-    return arr
-  }, [])
+function creatingArr(arr) {
+  let data = JSON.parse(localStorage.getItem('cart'))
+  data = data ? data : arr
+  localStorage.setItem('cart',JSON.stringify(data))
+  // data.reduce((arr) => {
+  //   arr.push(new Array)
+  //   return arr
+  // }, [])
 
+}
+
+function creatingProdCOuntObj(){
+
+}
+
+
+function showinSingelProductCount(id,count){
+  let show = document.querySelectorAll('.products___add')
+  let singelProdCount={}
+  show.forEach(item=>{
+    let domId = item.parentElement.parentElement.id
+    if(domId==id){
+     item.innerHTML=count
+     singelProdCount[`${domId}`]=count
+    }
+  
+  })
+  console.log(singelProdCount)
+}
+
+
+
+
+ 
+function addToCart(wrapper, cartCount,total,arr){
+  wrapper.forEach(item => {
+    item.addEventListener('click', (e) => {
+      let currentId = e.target.parentElement.parentElement.id
+      let parent = e.target.parentElement.parentElement
+      let pushObj = {
+        image: parent.firstElementChild.firstElementChild.getAttribute('src'),
+        name: parent.firstElementChild.nextElementSibling.textContent,
+        price: parent.lastElementChild.previousElementSibling.textContent
+      }
+
+
+
+      if (e.target.className == 'products__minus') {
+
+        let cartData = JSON.parse(localStorage.getItem('cart'))
+        cartData = cartData ? cartData : arr
+        cartData[parseInt(currentId)].pop(pushObj)
+        localStorage.setItem('cart', JSON.stringify(cartData))
+        showinSingelProductCount(currentId, cartData[currentId].length)
+
+        let currentCount = parseInt(localStorage.getItem('totalProd'))
+        currentCount = currentCount ? currentCount : total
+        total = findingLength(cartData)
+        cartCount.innerHTML = total
+        localStorage.setItem('totalProd', JSON.stringify(total))
+
+      } else {
+
+        let cartData = JSON.parse(localStorage.getItem('cart'))
+        cartData = cartData ? cartData : arr
+        cartData[parseInt(currentId)].push(pushObj)
+        showinSingelProductCount(currentId, cartData[currentId].length)
+        localStorage.setItem('cart', JSON.stringify(cartData))
+
+        let currentCount = parseInt(localStorage.getItem('totalProd'))
+        currentCount = currentCount ? currentCount : total
+        total = findingLength(cartData)
+        cartCount.innerHTML = total
+        localStorage.setItem('totalProd', JSON.stringify(total))
+
+      }
+    })
+
+  })
 }
 
 
@@ -98,76 +173,24 @@ function creatingArr() {
 
 
 
-
-
-
-wrapper.forEach(item=>{
-  item.addEventListener('click', (e) => {
-    let currentId = e.target.parentElement.parentElement.id 
-    let parent = e.target.parentElement.parentElement
-    let pushObj={
-      image: parent.firstElementChild.firstElementChild.getAttribute('src'),
-      name: parent.firstElementChild.nextElementSibling.textContent,
-      price: parent.lastElementChild.previousElementSibling.textContent
-    }
-
-   
-
-     if( e.target.className == 'products__minus'){
-
-       let cartData = JSON.parse(localStorage.getItem('cart'))
-       cartData = cartData ? cartData : arr
-       cartData[parseInt(currentId)].pop(pushObj)
-       localStorage.setItem('cart', JSON.stringify(cartData))
-
-       let currentCount=parseInt(localStorage.getItem('totalProd'))
-       currentCount = currentCount ? currentCount:total
-        total = findingLength(cartData)
-        cartCount.innerHTML = total
-        localStorage.setItem('totalProd', JSON.stringify(total))
-
-        }else{
-
-       let cartData = JSON.parse(localStorage.getItem('cart'))
-       cartData = cartData ? cartData:arr
-       cartData[parseInt(currentId)].push(pushObj)
-       console.log(cartData)
-       localStorage.setItem('cart', JSON.stringify(cartData))
-
-       let currentCount = parseInt(localStorage.getItem('totalProd'))
-       currentCount = currentCount ? currentCount : total
-       total = findingLength(cartData)
-       cartCount.innerHTML = total
-       localStorage.setItem('totalProd', JSON.stringify(total))
-     
-     }
+function searchItems(search, productsCard){
+   search.addEventListener('keyup', (e) => {
+    let userInput = e.target.value.toLowerCase()
+    productsCard.forEach(item => {
+      let text = item.firstElementChild.nextElementSibling.innerText.toLowerCase()
+      if (text.includes(userInput)) {
+        item.style.display = 'block'
+      }
+      if (!text.includes(userInput)) {
+        item.style.display = 'none'
+      }
     })
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-search.addEventListener('keyup',(e)=>{
-  let userInput=e.target.value.toLowerCase()
-  products.forEach(item=>{
-   let text=item.firstElementChild.nextElementSibling.innerText.toLowerCase()
-    if (text.includes(userInput)){
-      item.style.display = 'block'
-    }
-    if (!text.includes(userInput)){
-      item.style.display='none'
-    }
   })
-})
+}
+
+
+
+
 
 
 
